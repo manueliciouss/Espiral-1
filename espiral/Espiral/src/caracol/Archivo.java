e9c1bc5a6b762f13515e0356d36e667b95573e83
manueliciouss <42239557+manueliciouss@users.noreply.github.com>
2019-02-14 06:43:09 -0500

    Update Achivo.java
    se corrige error

--- espiral/Espiral/src/caracol/Archivo.java
+++ espiral/Espiral/src/caracol/Archivo.java
@@ -5,32 +5,32 @@ import java.io.*;
 
 public class Archivo {
 
-   public void leer(String Archivo){
-       
-       int k = 0;
-       String[] vector = null;
-         
-       try {
+    public void leer(String Archivo) {
+
+        int k = 0;
+        String[] vector = null;
+
+        try {
             FileReader r = new FileReader(Archivo);
             BufferedReader buffer = new BufferedReader(r);
-           
+
             String temp = "";
             boolean f = true;
-            
-            while(temp != null){
+
+            do  {
                 k++;
                 temp = buffer.readLine();
+                if(temp==null) break;
                 String x;
                 x = temp;
                 vector = x.split(" ");
-            Mover e = new Mover(vector);
-            e.reloj();
-            }
-            
+
+                Mover e = new Mover(vector);
+                e.reloj();
+            }while((temp != null));
+
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("fin " + e);
         }
-        
-   }
-    
+    }
 }
